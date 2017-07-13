a57a1a873cc7a9abd81cbb566da09ac51e86300d
Student <Student@STEM205.jcsu.edu>
2017-07-13 17:06:47 -0400

    Added some new templates.

--- RoomE/main.py
+++ RoomE/main.py
@@ -7,19 +7,18 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader("templates"))
 
-class Users (ndb.Model):
+class User (ndb.Model):
     Full_Name= ndb.StringProperty()
-    Email = ndb.StringProperty()
+    Username = ndb.StringProperty()
     Gender= ndb.StringProperty()
-
 
 class SignupHandler(webapp2.RequestHandler):
     def get(self):
      template = jinja_environment.get_template('sign_up.html')
      self.response.out.write(template.render())
-     user= Users()
+     user= User()
      user.Full_Name= self.request.get("name")
-     user.Email= self.request.get("email")
+     user.Username= self.request.get("Username")
      user.Gender= self.request.get("gender")
      user.put()
 
@@ -29,7 +28,16 @@ class HomeHandler(webapp2.RequestHandler):
      self.response.out.write(template.render())
 
 
+class Preferences(ndb.Model):
 
+     username=ndb.StringProperty()
+     smoke= ndb.BooleanProperty()
+     late_riser= ndb.BooleanProperty()
+     snore=ndb.BooleanProperty()
+     activity_level= ndb.StringProperty()
+     sexuality= ndb.StringProperty()
+     religion= ndb.StringProperty()
+     religion_imp= ndb.StringProperty()
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
@@ -39,27 +47,52 @@ class LoginHandler(webapp2.RequestHandler):
     def post(self):
 
         username= self.request.get("username")
-        query= Users.query(Users.Email==username)
+
+        query= User.query(User.Username==username)
         user= query.get()
         username_value= self.request.cookies.get('get_username')
 
+        if user:
 
-        self.response.set_cookie('get_username', user.Email, max_age=360, path='/')
+            self.response.set_cookie('get_username', user.Username, max_age=360,
+            path='/')
+
+            # self.response.write("You're Logged In!")
+            #
+            self.redirect("/profile")
+
+        else:
+
+            self.response.write("Unrecognized Username")
 
 
-
-
-        self.response.write("You're Logged In!")
 
 class PrefHandler2(webapp2.RequestHandler):
     def post(self):
-        template = jinja_environment.get_template('start_2.html')
+        User_Preferences= Preferences()
+
+        fields= ["smoke", "late_riser", "snore", ]
+
+
+        for field in fields:
+
+            valid= self.request.get(field)=="yes"
+            setattr(User_Preferences, field, valid)
+
+            Traits=[ ]
+
+
+        User_Preferences.username=self.request.get("username")
+        User_Preferences.activity_level=self.request.get("activity_level")
+        User_Preferences.sexuality=self.request.get("sexuality")
+        User_Preferences.religion= self.request.get("religion")
+        User_Preferences.religion_imp= self.request.get("religion_imp")
+        User_Preferences.put()
+
+
+
+        template = jinja_environment.get_template('/start_2.html')
         self.response.out.write(template.render())
-        User_Preferences= Users()
-        User_Preferences.Full_Name= self.request.get("name")
-        User_Preferences.Email= self.request.get("email")
-        User_Preferences.Gender= self.request.get("gender")
-        user.put()
 
 
 class MainPage(webapp2.RequestHandler):
@@ -76,14 +109,25 @@ class MainPage(webapp2.RequestHandler):
 
 class PrefHandler(webapp2.RequestHandler):
     def post(self):
+        tpl_dict={"username": self.request.get("Username")}
         template = jinja_environment.get_template('start.html')
-        self.response.out.write(template.render())
-        user= Users()
+        self.response.out.write(template.render(tpl_dict))
+        user= User()
         user.Full_Name= self.request.get("name")
-        user.Email= self.request.get("email")
+        user.Username= self.request.get("Username")
         user.Gender= self.request.get("gender")
         user.put()
 
+
+class ProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        username_value= self.request.cookies.get('get_username')
+        template = jinja_environment.get_template('profile.html')
+        user_preferences= Preferences.query(Preferences.username==username_value)
+        display_user_preferences= user_preferences.get()
+        ## self.response.write(display_user_preferences)
+
+        self.response.out.write(template.render({"Profile": username_value, "Profile_Info":display_user_preferences}))
 
 
 
@@ -102,6 +146,6 @@ app = webapp2.WSGIApplication([
     ('/home', MainPage),
     ("/pref", PrefHandler),
     ('/pref2', PrefHandler2),
-    ("/data", Users),
+    ('/profile', ProfileHandler)
 
 ], debug=True)
